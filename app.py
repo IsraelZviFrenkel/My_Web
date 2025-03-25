82ff8a72c119b442b6b5b854d7004b12b67f35a4
israe <lklkl57497@gmail.com>
2025-03-25 16:40:41 +0200

    quite done!!

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
 from flask import Flask, render_template, request, flash, url_for, redirect, session
+from functools import wraps
+
 import sqlite3
 
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.secret_key = "mysecretkey"
+app.secret_key = "my_secret_key"
 db_local = 'student.db'
 
 bcrypt = Bcrypt(app)
 
 
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            flash("You must be logged in to access this page", "danger")
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 @app.route('/home')
+@login_required
 def home_page():
     student_data = query_contact_details()
     return render_template('home.html', student_data=student_data)
 
 
-@app.route('/add', methods = ['GET', 'POST'])
+def query_contact_details():
+    connie = sqlite3.connect(db_local)
+    c = connie.cursor()
+    c.execute("""
+    SELECT * FROM contact_details
+    """)
+    student_data = c.fetchall()
+    return student_data
+
+
+@app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add_student():
     if request.method == 'GET':
         return render_template('add_student.html')
@@ -31,6 +60,7 @@ def add_student():
         insert_student(student_details)
         return render_template('add_success.html')
 
+
 def insert_student(student_details):
     connie = sqlite3.connect(db_local)
     c = connie.cursor()
@@ -39,15 +69,6 @@ def insert_student(student_details):
     connie.commit()
     connie.close()
 
-
-def query_contact_details():
-    connie = sqlite3.connect(db_local)
-    c = connie.cursor()
-    c.execute("""
-    SELECT * FROM contact_details
-    """)
-    student_data = c.fetchall()
-    return student_data
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -87,24 +108,35 @@ def login():
         if user and bcrypt.check_password_hash(user[0], password):
             session['user'] = username
             flash("Login successful!", "success")
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('home_page'))
         else:
             flash("Invalid username or password", "danger")
     return render_template('login.html')
 
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    if 'user' in session:
-        return f"<h2>Welcome, {session['user']}!</h2> <a href='/logout'</a>"
-    return redirect(url_for(('login')))
+    return f"<h2>Welcome, {session['user']}!</h2> <a href='/logout'</a>"
 
 
 @app.route('/logout')
 def logout():
+    # Remove the user from the session
     session.pop('user', None)
+
+    # Clear all session data
+    session.clear()
+
+    # Clear the session cookie
+    response = redirect(url_for('login'))
+    response.set_cookie('session', '', expires=0)  # Clear the session cookie
+
+    # Flash a logout success message
     flash("Logged out successfully!", "success")
-    return redirect(url_for('login'))
+
+    return response
+
 
 if __name__ == '__main__':
     app.run()
